c416979f5a942e5bdd2671aa5738804cc61752dd
Iffat Hossain <iffathossain@Iffats-MacBook-Air.local>
2022-04-08 13:50:02 -0400

    up

--- react-ui-pattern/src/Banner.jsx
+++ react-ui-pattern/src/Banner.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import Data from './App'
 
-function Banner(props) {
+// function Banner(props) {
   
 
-}
+// }
 
 
 
-return (
-  <div>
-    <img src="banners"></img>
-  </div>
+// return (
+//   <div>
+//     <img src="banners"></img>
+//   </div>
 
-)
+// )
